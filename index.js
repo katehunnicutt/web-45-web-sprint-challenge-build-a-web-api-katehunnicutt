616d10d239cd7ef9036410fbbf0057ed6844dbee
kait hunnicutt <katerenehunnicutt@gmail.com>
2021-09-03 14:24:42 -0700

    deploy

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-/*
-play this: https://www.youtube.com/watch?v=d-diB65scQU
+const express = require("express")
+const cors = require("cors")
 
-Sing along:
 
-here's a little code I wrote, please read the README word for word, don't worry, you got this
-in every task there may be trouble, but if you worry you make it double, don't worry, you got this
-ain't got no sense of what is REST? just concentrate on learning Express, don't worry, you got this
-your file is getting way too big, bring a Router and make it thin, don't worry, be crafty
-there is no data on that route, just write some code, you'll sort it out… don't worry, just hack it…
-I need this code, but don't know where, perhaps should make some middleware, don't worry, just hack it
+//const actionsRouter = require("./api/actions/actions-router")
+//const projectsRouter = require("./api/projects/projects-router")
+const PORT = process.env.PORT || 5000
 
-Pull your server into this file and start it!
-*/
+const server = express() 
+
+server.use(express.json())
+server.use(cors())
+
+server.get("/", (req, res) => {
+  res.send("<h1>ugh</h1>")
+})
+server.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`)
+})
+
+// module.exports = server
